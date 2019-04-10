8cf8780915bb3841c604dd27969f7d120f8d51c6
solgamd <solgamd@gmail.com>
2019-04-10 19:06:57 +0200

    Added a button and form; need to fix form code

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$('document').ready(function () {
+
+});
+
+$('#btnSubmit').click(function() {
+    alert('Hiya there!');
+})
+
+$('form').submit(function() {
+    alert.val(<input type="text">);
+})
